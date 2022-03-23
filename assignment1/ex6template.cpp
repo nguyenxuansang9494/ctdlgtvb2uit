4e005228e587bfc2676e1dc77f2fabcc5015226f
Sang Nguyen <nguyenxuansang9494@gmail.com>
2022-03-23 20:14:07 +0700

    remove comment

--- assignment1/ex6template.cpp
+++ assignment1/ex6template.cpp
@@ -1,11 +1,3 @@
-/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
-define
-include
-class
-[]
-template
-###End banned keyword*/
-
 #include <iostream>
 using namespace std;
 struct node
