a65d8b9a5f0b20b1dd6c2f4eafe7a554a4d309f6
Sang Nguyen <nguyenxuansang9494@gmail.com>
2022-03-24 20:22:38 +0700

    assignment 2

---
+++ assignment2/ex2template.cpp
@@ -0,0 +1,204 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+class
+[
+]
+template
+
+###End banned keyword*/
+
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+struct Node
+{
+    int value;
+    Node *next;
+};
+
+struct LinkedList
+{
+    Node *head;
+    Node *tail;
+    int size;
+};
+
+LinkedList init(int value)
+{
+    Node *node = new Node{value, nullptr};
+    return {node, node, 1};
+}
+
+LinkedList init()
+{
+    return {nullptr, nullptr, 0};
+}
+
+void add(LinkedList &linkedList, int index, int value)
+{
+    // index out of bound.
+    if (index > linkedList.size || index < 0)
+    {
+        return;
+    }
+    // if size of list is 0, just init a list with size 1 and assign it to the original list
+    if (linkedList.size == 0)
+    {
+        linkedList = init(value);
+        return;
+    }
+    // if index is 0, assign a new node point to old head node to old head node
+    if (index == 0)
+    {
+        linkedList.head = new Node{value, linkedList.head};
+        linkedList.size++;
+        return;
+    }
+    // if index equals to size, add a new node after tail and assign the new node to the tail
+    if (index == linkedList.size)
+    {
+        linkedList.tail->next = new Node{value, nullptr};
+        linkedList.tail = linkedList.tail->next;
+        linkedList.size++;
+        return;
+    }
+    // for loop to traverse to the node at the index
+    Node *current = linkedList.head;
+    for (int i = 1; i < index; i++)
+    {
+        current = current->next;
+    }
+    // add the new node at the (index)th position
+    current->next = new Node{value, current->next};
+    // increment the list size
+    linkedList.size++;
+}
+
+void addHead(LinkedList &linkedList, int value)
+{
+    add(linkedList, 0, value);
+}
+
+void addTail(LinkedList &linkedList, int value)
+{
+    add(linkedList, linkedList.size, value);
+}
+
+void remove(LinkedList &linkedList, int index)
+{
+    if (index >= linkedList.size || index < 0)
+    {
+        return;
+    }
+    if (index == 0)
+    {
+        Node *newhead = linkedList.head->next;
+        delete linkedList.head;
+        linkedList.head = newhead;
+        linkedList.size--;
+        return;
+    }
+    Node *current = linkedList.head, *prevCurrent;
+    for (int i = 1; i <= index; i++)
+    {
+        prevCurrent = current;
+        current = current->next;
+    }
+    prevCurrent->next = current->next;
+    if (index == linkedList.size - 1)
+    {
+        linkedList.tail = prevCurrent;
+    }
+    delete current;
+    linkedList.size--;
+}
+
+Node *get(LinkedList linkedList, int index)
+{
+    if (index < 0 || index >= linkedList.size)
+    {
+        return nullptr;
+    }
+    Node *current = linkedList.head;
+    for (int i = 1; i <= index; i++)
+    {
+        current = current->next;
+    }
+    return current;
+}
+
+int find(LinkedList linkedList, int value)
+{
+    Node *current = linkedList.head;
+    int index = 0;
+    while (current != nullptr)
+    {
+        if (current->value == value)
+        {
+            return index;
+        }
+        index++;
+        current = current->next;
+    }
+    return -1;
+}
+
+LinkedList removeDupplicateValue(LinkedList linkedList)
+{
+    LinkedList result = init();
+    Node *current = linkedList.head;
+    while (current != nullptr)
+    {
+        if (find(result, current->value) == -1)
+            addTail(result, current->value);
+        current = current->next;
+    }
+    return result;
+}
+
+void printOutList(LinkedList linkedList)
+{
+    Node *current = linkedList.head;
+    while (current != nullptr)
+    {
+        std::cout << current->value << " ";
+        current = current->next;
+    }
+    std::cout << "\n";
+}
+
+void reversePrintOutList(Node *node)
+{
+    if (node->next != nullptr)
+    {
+        reversePrintOutList(node->next);
+    }
+    std::cout << node->value << " ";
+}
+
+int main(int argc, char const *argv[])
+{
+    LinkedList linkedList = init();
+    int n;
+    while (true)
+    {
+        std::cin >> n;
+        if (n == 0)
+            break;
+        addHead(linkedList, n);
+    }
+    if (linkedList.size == 0)
+    {
+        std::cout << "Danh sach rong.\n";
+        return 0;
+    }
+    printOutList(linkedList);
+    linkedList = removeDupplicateValue(linkedList);
+    printOutList(linkedList);
+    return 0;
+}
